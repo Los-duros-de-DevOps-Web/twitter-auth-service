6428c4ae085fe1fb7045efed57f2d33af329fb43
GabrielSB19 <gasuarez2002@gmail.com>
2023-10-10 11:28:01 -0500

    Added the auth in express

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+
+const app = express();
+app.use(express.json());
+
+const jwt = require("jsonwebtoken");
+
+const prisma = require("./libs/prisma");
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user)
+    return res.status(400).json({ error: "Invalid email or password" });
+
+  const validPassword = await bcrypt.compare(password, user.hashedPassword);
+  if (!validPassword)
+    return res.status(400).json({ error: "Invalid email or password" });
+
+  const uId = user.id;
+  const uName = user.name;
+
+  const token = jwt.sign(
+    { uId, uName, exp: Date.now() + 60 * 1000 },
+    process.env.NEXTAUTH_JWT_SECRET
+  );
+  res.json({ token });
+});
+
+app.listen(3002, () => {
+  console.log("Server is running on port 3002.");
+});
---
+++ libs/prisma.js
@@ -0,0 +1,7 @@
+const { PrismaClient } = require("@prisma/client");
+
+global.prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") global.prisma = global.prisma;
+
+module.exports = global.prisma;
